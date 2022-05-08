d337999d4d882f47b2e0b0094d08177f59aeca5e
Alex H. Kwon <khwalex@gmail.com>
2022-05-08 19:42:12 +0900

    빌드 에러 해결 (#22)
    - https://www.notion.so/greengablesstudio/f0f8087a21f04895909eafc9dc4a4190

--- src/_legacy/components/header/HeaderXIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Icon from '../icon/Icon';
-import {ICON_TYPE} from '../icon/Icon';
-import XSVG from '../svg/XSVG';
-
-export default function HeaderXIcon() {
-  return <Icon type={ICON_TYPE[28]} svg={<XSVG />} />;
-}
--- src/_legacy/components/ui/CartShoppingIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Icon from '../icon/Icon';
-import CartShoppingSVG from '../svg/CartShoppingSVG';
-
-export default function CartShoppingIcon() {
-  return (
-    <Icon svg={<CartShoppingSVG className="h-5 hover:opacity-80" />}></Icon>
-  );
-}
