997334da6d04dfd0635afa3ac1728c44e2a5a4d0
Alex H. Kwon <khwalex@gmail.com>
2022-05-08 10:36:49 +0900

    Feature/hydrogen migration (#21)
    * Hydrogen 0.17.2 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.15.0 업데이트
    
    * Hydrogen 0.17.2 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.16.0 업데이트
    
    * Hydrogen 0.17.2 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.16.1 업데이트
    
    * Hydrogen 0.17.2 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.17.0 업데이트
    
    * Hydrogen 0.17.2 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.17.1 업데이트
    
    * Hydrogen 0.17.2 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.17.2 업데이트
    
    * Hydrogen 0.17.3 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c
    - 0.17.3 업데이트
    
    * Hydrogen 0.17.3 업데이트
    
    - https://www.notion.so/greengablesstudio/Hydrogen-0-17-2-c8bc4cdf4e37422796c1c06042d41e4c

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -1,7 +1,18 @@
 import renderHydrogen from '@shopify/hydrogen/entry-server';
-import {Router, Route, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
+import {
+  Router,
+  Route,
+  FileRoutes,
+  ShopifyProvider,
+  PerformanceMetricsServerAnalyticsConnector,
+  CookieSessionStorage,
+} from '@shopify/hydrogen';
 import {Suspense} from 'react';
 import shopifyConfig from '../shopify.config';
+import {
+  PerformanceMetrics,
+  PerformanceMetricsDebug,
+} from '@shopify/hydrogen/client';
 
 function App({routes}) {
   return (
@@ -11,6 +22,8 @@ function App({routes}) {
           <FileRoutes routes={routes} />
           <Route path="*" page={<>Not Found</>} />
         </Router>
+        <PerformanceMetrics />
+        {process.env.LOCAL_DEV && <PerformanceMetricsDebug />}
       </ShopifyProvider>
     </Suspense>
   );
@@ -18,4 +31,15 @@ function App({routes}) {
 
 const routes = import.meta.globEager('./routes/**/*.server.[jt](s|sx)');
 
-export default renderHydrogen(App, {shopifyConfig, routes});
+export default renderHydrogen(App, {
+  routes,
+  shopifyConfig,
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
