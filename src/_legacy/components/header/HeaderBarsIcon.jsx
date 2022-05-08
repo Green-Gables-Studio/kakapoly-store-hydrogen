2f8f41a05035a38d7f45e4d9a7b06fb1ab1f5f1a
Alex Kwon <khwalex@gmail.com>
2022-05-08 19:37:17 +0900

    빌드 에러 해결
    - https://www.notion.so/greengablesstudio/f0f8087a21f04895909eafc9dc4a4190

--- src/_legacy/components/header/HeaderBarsIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Icon from '../icon/Icon';
-import {ICON_TYPE} from '../icon/Icon';
-import BarsSVG from '../svg/BarsSVG';
-
-export default function HeaderBarsIcon() {
-  return <Icon type={ICON_TYPE[28]} svg={<BarsSVG />} />;
-}
