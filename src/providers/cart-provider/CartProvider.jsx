215d95ce8b0c89dc4946667679cf48588cf0f4a2
Alex H. Kwon <khwalex@gmail.com>
2022-05-12 22:36:19 +0900

    체크아웃 및 장바구니 담기 시 로딩 바 적용하기 (#23)
    - https://www.notion.so/greengablesstudio/e171375d3cf349ee836ebc9a71dd5305

--- src/providers/cart-provider/CartProvider.jsx
+++ src/providers/cart-provider/CartProvider.jsx
@@ -1,20 +1,9 @@
-import React, {createContext} from 'react';
+import React from 'react';
 import {CartProvider as ShopifyCartProvider} from '@shopify/hydrogen/client';
-import {useCartState} from '../cart-state-provider/CartStateProvider';
 
 export default function CartProvider({children, numCartLines}) {
-  const {openCart} = useCartState();
-
   return (
-    <ShopifyCartProvider
-      numCartLines={numCartLines}
-      onLineAdd={() => {
-        openCart();
-      }}
-      onCreate={() => {
-        openCart();
-      }}
-    >
+    <ShopifyCartProvider numCartLines={numCartLines}>
       {children}
     </ShopifyCartProvider>
   );
