0c4f19c72194be8c7277594c6a081a48a7b9a02f
Alex H. Kwon <khwalex@gmail.com>
2022-05-13 01:09:27 +0900

    Hydrogen 0.18.0 마이그레이션 (#24)
    - https://www.notion.so/greengablesstudio/Hydrogen-0-18-0-2e143348a898413d889045172e606206

---
+++ hydrogen.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+import {
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen';
+
+export default defineConfig({
+  routes: import.meta.globEager('./src/routes/**/*.server.[jt](s|sx)'),
+  shopify: {
+    storeDomain: 'kakapoly.myshopify.com',
+    storefrontToken: 'd1adb9b5aed22369844f76545b22d8bd',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
