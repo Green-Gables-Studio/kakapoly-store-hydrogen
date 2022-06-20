873413020954b299526847a138dd086c9195dc46
Alex Kwon <khwalex@gmail.com>
2022-06-20 23:09:07 +0900

    애널리틱스 툴 설치
    https://www.notion.so/greengablesstudio/26422e345e9b4cfdbb2760180488ee47

--- src/components/general/page-layout/PageLayout.tsx
+++ src/components/general/page-layout/PageLayout.tsx
@@ -4,6 +4,8 @@ import CartContainer from '../cart/CartContainer';
 import CartStateProvider from '../../../providers/cart-state-provider/CartStateProvider';
 import HeaderContainer from '../header/HeaderContainer';
 import Layout, {LAYOUT_TEMPLATE} from '../../ui/layout/Layout';
+import MetaPixel from '../meta-pixel/MetaPixel.client';
+import {GoogleAnalytics} from '../google-analytics/GoogleAnalytics.client';
 
 type Props = {
   children?: ReactNode;
@@ -12,17 +14,21 @@ type Props = {
 
 const PageLayout = ({template, children}: Props) => {
   return (
-    <ShopifyCartProvider numCartLines={undefined}>
-      <CartStateProvider>
-        <Layout
-          template={template}
-          header={<HeaderContainer />}
-          cart={<CartContainer />}
-        >
-          {children}
-        </Layout>
-      </CartStateProvider>
-    </ShopifyCartProvider>
+    <>
+      <ShopifyCartProvider numCartLines={undefined}>
+        <CartStateProvider>
+          <Layout
+            template={template}
+            header={<HeaderContainer />}
+            cart={<CartContainer />}
+          >
+            {children}
+          </Layout>
+        </CartStateProvider>
+      </ShopifyCartProvider>
+      <MetaPixel />
+      <GoogleAnalytics />
+    </>
   );
 };
 
